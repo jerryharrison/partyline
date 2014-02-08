2afdc9141112d89204870f9cd96b0cd23ddcbc89
Jerry Harrison <jerry@indieventuresllc.com>
2014-02-08 18:38:43 -0500

    adding package.json and server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+
+var express = require('express');
+var simplesmtp = require("simplesmtp");
+var fs = require("fs");
+
+var smtp = simplesmtp.createServer().listen(25);
+
+smtp.on("startData", function(connection){
+    console.log("Message from:", connection.from);
+    console.log("Message to:", connection.to);
+    // connection.saveStream = fs.createWriteStream("/tmp/message.txt");
+});
+
+/*
+smtp.on("data", function(connection, chunk){
+    connection.saveStream.write(chunk);
+});
+
+smtp.on("dataReady", function(connection, callback){
+    connection.saveStream.end();
+    console.log("Incoming message saved to /tmp/message.txt");
+    callback(null, "ABC1"); // ABC1 is the queue id to be advertised to the client
+    // callback(new Error("Rejected as spam!")); // reported back to the client
+});
+*/
