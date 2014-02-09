d5bcad5b2cd1cbc1c35ac847c8b00fc020e8db98
Jerry Harrison <jerry@indieventuresllc.com>
2014-02-09 01:00:56 -0500

    added simple http test server

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ smtp.on("startData", function(connection){
     // connection.saveStream = fs.createWriteStream("/tmp/message.txt");
 });
 
+
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(80);
+console.log('Server running at http://127.0.0.1:1337/');
+
 /*
 smtp.on("data", function(connection, chunk){
     connection.saveStream.write(chunk);
