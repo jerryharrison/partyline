6c3e13260f71667eb2757ce1c51cbcd02619aa5b
Jerry Harrison <jerry@indieventuresllc.com>
2014-02-10 16:06:26 -0500

    Changed the http server welcome message.

--- http_server.js
+++ http_server.js
@@ -3,6 +3,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Welcome to Partyline! Join for free simply email us at signup@partyline.cc with the participants as the CC and we\'ll setup the forward.');
+  res.end('Welcome to Partyline!\r\nJoin for free.\r\nSimply email [group_name]@partyline.cc (ex. stjameschurch@partyline.cc) with the participants as the CC and we\'ll setup the rest.\r\nFrom this point on everyone in your partyline can simply email your group\'s partyline and you\'re good to go\r\n\r\nHappy Group Emailin\'');
 }).listen(80);
 console.log('HTTP Server is running at http://partyline.cc/');
