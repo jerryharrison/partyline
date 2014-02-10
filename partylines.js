d35763c8adf9b1138fd32d693db17bf34a98dfd8
Jerry Harrison <jerry@indieventuresllc.com>
2014-02-10 15:48:02 -0500

    adding external partylines

---
+++ partylines.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  bni: [
+    {email: 'fivesecondrule@gmail.com', name: 'Jerry Harrison', type: 'to'},
+    {email: 'jerry@lunadesk.com', name: 'Jerry Luna', type: 'to'},
+    {email: 'harrison@grandstrandlawyers.com', name: 'Angela Harrison', type: 'to'}
+  ],
+  test: [
+    {email: 'fivesecondrule@gmail.com', name: 'Jerry Harrison', type: 'to'},
+    {email: 'jerry@lunadesk.com', name: 'Jerry Luna', type: 'to'}
+  ]
+};
